282a7d732ef1ad5ca7ec97145dc7109a660aca4b
NehaBaral <41846320+NehaBaral@users.noreply.github.com>
2024-10-24 00:15:18 -0400

    About page updated

--- app/src/main/java/com/example/android_project1/ui/about/AboutActivity.kt
+++ app/src/main/java/com/example/android_project1/ui/about/AboutActivity.kt
@@ -4,12 +4,19 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.android_project1.R
+import com.example.android_project1.databinding.ActivityAboutBinding
 
 class AboutActivity : AppCompatActivity() {
+    lateinit var binding : ActivityAboutBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_about)
+        binding = ActivityAboutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.nextBtn.setOnClickListener {
+
+        }
 
     }
 }
